1232d9f20bbf398e2e14ba87e3740fbe75a94434
Ahamad-PC <ahamad.jedu@gmail.com>
2018-10-02 06:54:52 +0700

    leave president

---
+++ controllers/PresidentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace andahrm\leave\controllers;
+
+class PresidentController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
---
+++ views/president/index.php
@@ -0,0 +1,7 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1>president/index</h1>
+
+
+<div class="alert alert-warning">Coming soon</div>
